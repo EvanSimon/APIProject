1189c057d80ac6d42584c91b2c6ea53c94ac48bd
KendraDi <kefele@aim.com>
2019-02-25 16:26:17 -0500

    Validation Dev

--- RecipesProjects/Models/UserInput.cs
+++ RecipesProjects/Models/UserInput.cs
@@ -1,17 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 namespace RecipesProjects.Models
 {
+
+   
     public class UserInput
     {
-
+        [Required]
+        [DataType(DataType.Text)]
         public string MovieName { get; set; }
-        
 
+        
         public UserInput()
-        { }
+        {
+           
+        }
+
+
     }
 }
